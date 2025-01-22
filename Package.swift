83e82b2d07380a6a69e745f93e9579153ec3dda4
Marius Koch <marius.koch@gematik.de>
2025-01-22 21:20:51 +0100

    update dependencies

--- Package.swift
+++ Package.swift
@@ -20,6 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
                     name: "gemHealthKitToFHIR",
+                    dependencies: [
+                        .product(name: "ModelsR4", package: "FHIRModels")
+                    ],
                     path: "Sources/gemHealthKitToFHIR"),
         .testTarget(
             name: "gemHealthKitToFHIRTests",
